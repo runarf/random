5438ba8c85374e64074f6a406bea34e0e4cec77c
runar <runar@tuta.io>
2020-05-09 15:12:11 +0200

    Many changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "antd/dist/antd.css";
-import { Checkbox, Input, Button } from "antd";
+import { Checkbox, Input, Button, Row, Layout } from "antd";
 import { CheckboxValueType } from "antd/lib/checkbox/Group";
 import { CheckboxChangeEvent } from "antd/lib/checkbox";
 import Text from "antd/lib/typography/Text";
+import difference from "lodash/difference";
 
 const CheckboxGroup = Checkbox.Group;
 
@@ -19,6 +20,7 @@ const App = () => {
   const [currentlyChecked, setCurrentlyChecked] = useState<CheckboxValueType[]>(
     []
   );
+  const [candidates, setCandidates] = useState<CheckboxValueType[]>([]);
   const [state, setState] = useState<State>({
     indeterminate: true,
     isAllChecked: false,
@@ -31,23 +33,38 @@ const App = () => {
   useEffect(() => {
     const savedCheckedOptionsJson =
       localStorage.getItem(SAVED_CHECKED_OPTIONS) ?? "[]";
-    const savedCheckedOptions = JSON.parse(savedCheckedOptionsJson);
+    const savedCheckedOptions = JSON.parse(
+      savedCheckedOptionsJson
+    ) as CheckboxValueType[];
+
     setCurrentlyChecked(savedCheckedOptions);
 
     const savedAllOptionsJson = localStorage.getItem(SAVED_ALL_OPTIONS) ?? "[]";
     const savedAllOptions = JSON.parse(savedAllOptionsJson);
     setAllOptions(savedAllOptions);
+    const inputText = savedAllOptions.join(", ");
+    console.log(inputText);
+    setInputText(inputText);
+
+    if (savedAllOptions.length === savedCheckedOptions.length) {
+      setState({ indeterminate: false, isAllChecked: true });
+    } else if (savedCheckedOptions.length === 0) {
+      setState({ indeterminate: false, isAllChecked: false });
+    }
   }, []);
 
   useEffect(() => {
     const allOptionsJson = JSON.stringify(allOptions);
+
     localStorage.setItem(SAVED_ALL_OPTIONS, allOptionsJson);
 
     const currentlyCheckedJson = JSON.stringify(currentlyChecked);
     localStorage.setItem(SAVED_CHECKED_OPTIONS, currentlyCheckedJson);
+
+    setCandidates(currentlyChecked);
   }, [currentlyChecked, allOptions]);
 
-  const onChange = (checkedValues: CheckboxValueType[]) => {
+  const onClickCheckbox = (checkedValues: CheckboxValueType[]) => {
     const indeterminate =
       checkedValues.length > 0 && checkedValues.length < allOptions.length;
     setState({
@@ -57,11 +74,19 @@ const App = () => {
     setCurrentlyChecked(checkedValues);
   };
 
-  const onPressEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    const newChecked = inputText.split(",").map((text) => text.trim());
-    setAllOptions([...allOptions, ...newChecked]);
-    setCurrentlyChecked([...currentlyChecked, ...newChecked]);
-    setInputText("");
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputText = event.target.value;
+    setInputText(inputText);
+
+    const inputTextAllOptions = inputText.split(",").map((text) => text.trim());
+
+    const lengthAllOptions = allOptions.length;
+    setAllOptions(inputTextAllOptions);
+    if (inputTextAllOptions.length > lengthAllOptions) {
+      const diffAllOptions = difference(allOptions, inputTextAllOptions);
+      const diffCheckedOptions = difference(diffAllOptions, currentlyChecked);
+      setCurrentlyChecked([...currentlyChecked, ...diffCheckedOptions]);
+    }
   };
 
   const onCheckMasterChange = (e: CheckboxChangeEvent) => {
@@ -77,36 +102,49 @@ const App = () => {
     array[Math.floor(Math.random() * array.length)];
 
   const onClickButton = () => {
-    const randomPerson = getRandomElement(currentlyChecked) as string;
+    let localCandidates: CheckboxValueType[];
+    if (candidates.length > 0) {
+      localCandidates = candidates;
+    } else {
+      console.log("restart");
+      localCandidates = currentlyChecked;
+    }
+    const randomPerson = getRandomElement(localCandidates) as string;
     setRandomPerson(randomPerson);
+    setCandidates(difference(localCandidates, [randomPerson]));
   };
 
+  const { Content } = Layout;
   return (
-    <div>
-      <div>
-        <Checkbox
-          indeterminate={state.indeterminate}
-          onChange={onCheckMasterChange}
-          checked={state.isAllChecked}
-        >
-          Check all
-        </Checkbox>
-      </div>
-      <br />
-      <CheckboxGroup
-        options={allOptions}
-        value={currentlyChecked}
-        onChange={onChange}
-      />
-      <Input
-        placeholder="Basic usage"
-        onPressEnter={onPressEnter}
-        onChange={(e) => setInputText(e.target.value)}
-        value={inputText}
-      />
-      <Button onClick={onClickButton}>Get Random</Button>
-      <Text>{randomPerson}</Text>
-    </div>
+    <Layout>
+      <Content>
+        <Row>
+          <Checkbox
+            indeterminate={state.indeterminate}
+            onChange={onCheckMasterChange}
+            checked={state.isAllChecked}
+          >
+            Check all
+          </Checkbox>
+        </Row>
+        <Row>
+          <CheckboxGroup
+            options={allOptions}
+            value={currentlyChecked}
+            onChange={onClickCheckbox}
+          />
+        </Row>
+        <Input
+          placeholder="Basic usage"
+          onChange={onChangeInput}
+          value={inputText}
+          onPressEnter={() => setInputText(inputText + ", ")}
+        />
+        <Button onClick={onClickButton}>Get Random</Button>
+
+        <Text>{randomPerson}</Text>
+      </Content>
+    </Layout>
   );
 };
 
