fa35a3d6183ec7bcb657403b7662bf16880bf454
runar <runar@tuta.io>
2020-05-09 14:17:18 +0200

    localstorage for storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "antd/dist/antd.css";
 import { Checkbox, Input, Button } from "antd";
 import { CheckboxValueType } from "antd/lib/checkbox/Group";
@@ -12,10 +12,12 @@ interface State {
   isAllChecked: boolean;
 }
 
+const SAVED_CHECKED_OPTIONS = "savedCheckedOptions";
+const SAVED_ALL_OPTIONS = "savedAllOptions";
 const App = () => {
-  const [allOptions, setAllOptions] = useState(["Apple", "Pear", "Orange"]);
+  const [allOptions, setAllOptions] = useState<string[]>([]);
   const [currentlyChecked, setCurrentlyChecked] = useState<CheckboxValueType[]>(
-    ["Apple", "Orange"]
+    []
   );
   const [state, setState] = useState<State>({
     indeterminate: true,
@@ -25,6 +27,25 @@ const App = () => {
   const [inputText, setInputText] = useState("");
 
   const [randomPerson, setRandomPerson] = useState("");
+
+  useEffect(() => {
+    const savedCheckedOptionsJson =
+      localStorage.getItem(SAVED_CHECKED_OPTIONS) ?? "[]";
+    const savedCheckedOptions = JSON.parse(savedCheckedOptionsJson);
+    setCurrentlyChecked(savedCheckedOptions);
+
+    const savedAllOptionsJson = localStorage.getItem(SAVED_ALL_OPTIONS) ?? "[]";
+    const savedAllOptions = JSON.parse(savedAllOptionsJson);
+    setAllOptions(savedAllOptions);
+  }, []);
+
+  useEffect(() => {
+    const allOptionsJson = JSON.stringify(allOptions);
+    localStorage.setItem(SAVED_ALL_OPTIONS, allOptionsJson);
+
+    const currentlyCheckedJson = JSON.stringify(currentlyChecked);
+    localStorage.setItem(SAVED_CHECKED_OPTIONS, currentlyCheckedJson);
+  }, [currentlyChecked, allOptions]);
 
   const onChange = (checkedValues: CheckboxValueType[]) => {
     const indeterminate =
